0bd8914b7cd9e1eca050208300b213756fbc99fb
nkumar007 <naveen.mbuff98@gmail.com>
2023-04-13 21:43:03 +0530

    Deleting notes

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ class App {
       this.handleFormClick(event);
       this.selectNote(event);
       this.openModal(event);
+      this.deleteNote(event);
     });
 
     document.body.addEventListener("mouseover", (event) => {
@@ -103,6 +104,8 @@ class App {
   }
 
   openModal(event) {
+    if (event.target.matches(".toolbar-delete")) return;
+
     if (event.target.closest(".note")) {
       this.$modal.classList.toggle("open-modal");
       this.$modalTitle.value = this.title;
@@ -117,7 +120,7 @@ class App {
 
   openTooltip(event) {
     if (!event.target.matches(".toolbar-color")) return;
-    this.id = event.target.dataset.id;
+    this.id = event.target.nextElementSibling.dataset.id;
     const noteCoords = event.target.getBoundingClientRect();
     const horizontal = noteCoords.left;
     const vertical = window.scrollY - 20;
@@ -167,6 +170,14 @@ class App {
     this.id = $selectedNote.dataset.id;
   }
 
+  deleteNote(event) {
+    event.stopPropagation();
+    if (!event.target.matches(".toolbar-delete")) return;
+    const id = event.target.dataset.id;
+    this.notes = this.notes.filter((note) => note.id !== Number(id));
+    this.displayNotes();
+  }
+
   displayNotes() {
     const hasNotes = this.notes.length > 0;
     this.$placeholder.style.display = hasNotes ? "none" : "flex";
@@ -184,7 +195,9 @@ class App {
               <img class="toolbar-color" data-id=${
                 note.id
               } src="https://icon.now.sh/palette">
-              <img class="toolbar-delete" src="https://icon.now.sh/delete">
+              <img data-id=${
+                note.id
+              } class="toolbar-delete" src="https://icon.now.sh/delete">
             </div>
           </div>
         </div>
